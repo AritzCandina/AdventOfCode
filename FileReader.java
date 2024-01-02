a3c0ee4af3f24544fc30a97f324e56aa1d63366e
Lukas Jaiczay <lukas.jaiczay@stud.fh-campuswien.ac.at>
2024-01-02 23:45:52 +0100

    initial commit added findLastNumber

--- FileReader.java
+++ FileReader.java
@@ -40,13 +40,13 @@ public class FileReader {
 
     public int getNumbers(String line){
         char numFirst ='a';
-        char numLast = 'a';
+
         String completeNumber ="";
         int finalSum;
         String line2 = "";
         int posChar = 0;
 
-        /* get the first Digit in the string */
+        // get the first Digit in the string */
         for (int i = 0; i < line.length(); i++) {
             char c = line.charAt(i);
 
@@ -57,10 +57,10 @@ public class FileReader {
                 break;
             }
         }
-
-        /* Create a new string without the first Digit */
+/*
+        // Create a new string without the first Digit
         for (int i = 0; i < line.length(); i++) {
-            /* Omit the Digit that has been already added */
+            // Omit the Digit that has been already added
             if (i == posChar) {
                 i++;
                 continue;
@@ -72,29 +72,50 @@ public class FileReader {
 
         //System.out.println(line2);
 
-        /* get the last Digit of the new string */
+        // get the last Digit of the new string
         for (int i = line2.length() -1; i >= 0; i--){
 
             if (line2.charAt(i) >= '0' && line2.charAt(i) <= '9') {
                 numLast= line2.charAt(i);
-                //System.out.println("segundo for: "+ numLast);
+
                 break;
             }
         }
+        */
+
+
+
 
         if (numFirst != 'a') {
             completeNumber += numFirst;
         }
-        if (numLast != 'a'){
-            completeNumber += numLast;
-        }
+        int numLast = Integer.parseInt(findLastNumber(line));
+
+        completeNumber += numLast;
+
         finalSum = parseInt(completeNumber);
         return finalSum;
     }
 
+    public static boolean isNumeric(String line) {
+        return line.matches("-?\\d+(\\.\\d+)?");
+    }
+    public static String findLastNumber(String line) {
+        for (int i = line.length() - 1; i >= 0; i--) {
+            String currentChar = String.valueOf(line.charAt(i));
+            System.out.println(currentChar);
+            if (isNumeric(String.valueOf(currentChar))) {
+                return currentChar;
+
+
+            }
+        }
+        return "";
+    }
+
     public static void main(String[] args) {
 
-        FileReader reader = new FileReader("C:/IntelliJProjects/adventOfCode.txt");
+        FileReader reader = new FileReader("C:\\Users\\Lukas\\IdeaProjects\\AdventOfCode_123\\input.txt");
         reader.getCalibration(reader.file);
 
     }
